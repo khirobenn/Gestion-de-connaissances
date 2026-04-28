2ef478263d6785c0bd6c83c7ec0bf13bb33949c7
khirobenn <khireddine.benmeziane@etu.sorbonne-universite.fr>
2026-04-29 01:42:52 +0200

    Ajout de mémoire pour le LLM dans le contexte window

--- api.py
+++ api.py
@@ -18,10 +18,12 @@ origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+answers = []
 
 @app.get("/")
 async def root():
@@ -34,17 +36,24 @@ async def post(question_item:Item):
     return {"response":res}
     # return StreamingResponse(stream_generator(res), media_type='text/event-stream')
 
-async def stream_response(chain, information, question_item):
-    for chunk in chain.stream({"information": information, "question": question_item.question}):
+
+async def stream_response(chain, information, previous_discussion, question_item):
+    resp = ""
+    for chunk in chain.stream({"information": information, "previous_discussion": previous_discussion, "question": question_item.question}):
     #     yield json.dumps({"event": "message", "message": chunk})
+        resp += chunk
         yield chunk
         await asyncio.sleep(0.5)
+        answers.append({"question": question_item.question, "answer": resp})
     # yield json.dumps({"event" : "end"})
 
 @app.post("/stream/", response_class=StreamingResponse)
 async def stream(question_item:Item):
+    previous_discussion = ""
+    for disc in answers[-5:]:
+        previous_discussion += f"Question : {disc["question"]}\nAnswer : {disc["answer"]}\n"
     information = retriever.invoke(question_item.question)
-    return StreamingResponse(stream_response(chain, information, question_item), media_type="text/plain")
+    return StreamingResponse(stream_response(chain, information, previous_discussion, question_item), media_type="text/plain")
 
 if __name__ == "__main__":
     uvicorn.run("api:app", port=5000, log_level="info")
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
-
 model = OllamaLLM(model="gemma3:27b-cloud", streaming=True)
 
 template = """
-You are an expert in answering questions about football
+You are an expert in answering questions about champions league 2025/2026. If the question isn't about champions league, don't answer.
 
 Here are some information about what happened in champions league 2025/2026: {information}
+
+{previous_discussion}
 
 Here is the question to answer: {question}
 """
